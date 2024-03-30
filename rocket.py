4a1d2ada0471a6b4f5c923e66c69a0a39acf2714
Siddharth Grover <sgro0020@monash.student.edu>
2024-03-30 18:03:00 +0530

    added parameters for rocket mass and motor instance

--- rocket.py
+++ rocket.py
@@ -1,6 +1,11 @@
 import numpy
+from motor import Motor
 import matplotlib
 
 class Rocket:
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        mass: float, # kg
+        motor: Motor # Motor instance of the rocket
+    ) -> None:
         pass
