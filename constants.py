85465db1f496356f05baeb8abd626475eba619c8
Siddharth Grover <sgro0020@monash.student.edu>
2024-03-30 16:45:03 +0530

    added the constants g0, R, p0, T0, L

--- constants.py
+++ constants.py
@@ -3,4 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Constants:
-    pass
+    g0: float = 9.80665, # Mean gravitational acceleration at sea level (m/s^2)
+    R: float = 287.16, # Specfic gas constant for air
+    p0: int = 101325, # Standard sea level pressure (Pa)
+    T0: float = 288.15, # Standard sea level temperature (K)
+    L: float = -0.0065, # Lapse rate (K/m)
+
