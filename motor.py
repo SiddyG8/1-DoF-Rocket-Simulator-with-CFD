19b967293b12e11df0f8766932807c2466361293
Siddharth Grover <sgro0020@monash.student.edu>
2024-03-30 18:01:54 +0530

    created a motor class with parameters T0, tb, Isp

---
+++ motor.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Motor:
+    def __init__(
+        self,
+        T0: int, # Peak thrust (N)
+        tb: float, # Burnout time (s)
+        Isp: int # Specific impulse (s)
+    ) -> None:
+        pass
