2b6e455dfbaa435cccec1ee20f733feb2f480e23
Siddharth Grover <sgro0020@monash.student.edu>
2024-03-30 16:44:37 +0530

    added the drag coefficient function

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
-import numpy
-import matplotlib
+from numpy import e, sin, log10
 
 class Functions:
-    pass
+    def drag_coefficient(
+        M: float, # Mach number
+    ) -> float:
+        return pow(e, -1.2 * M) * sin(M) + (M / 6) * log10(M + 1)
 
 if __name__ == "__main__":
     pass
