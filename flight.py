1335f7b33d0d92874f9f51eee70b5a3c9f14b5c5
SiddyG8 <sgrov8@gmail.com>
2024-03-31 13:56:54 +0530

    created a flight class

---
+++ flight.py
@@ -0,0 +1,16 @@
+from rocket import Rocket
+from motor import Motor
+
+class Flight:
+    def __init__(
+        self,
+        rocket: Rocket,
+        t: float, # Time (s)
+        dt: float # Time step (s)
+    ) -> None:
+        pass
+    def simulate(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    pass
