33d525de6264c41ca93e3afe5e576776f88bb862
SiddyG8 <sgrov8@gmail.com>
2024-04-01 23:32:59 +0530

    converted the mass attribute into dry, wet, and fuel mass

--- rocket.py
+++ rocket.py
@@ -1,9 +1,14 @@
 from motor import Motor
 
 class Rocket:
-    def __init__(self, motor, mass) -> None:
+    def __init__(self, motor, wet_mass, fuel_mass) -> None:
         self.motor = motor
-        self.mass = mass
+        self.wet_mass = wet_mass
+        self.fuel_mass = fuel_mass
+
+    @property
+    def dry_mass(self) -> float:
+        return self.wet_mass - self.fuel_mass
 
 if __name__ == "__main__":
     pass
