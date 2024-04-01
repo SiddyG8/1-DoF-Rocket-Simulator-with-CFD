11a4faddbb1323c174291039c64be69ab89d9e3d
SiddyG8 <sgrov8@gmail.com>
2024-04-01 23:02:09 +0530

    DOC: added a docstring for the class

--- flight.py
+++ flight.py
@@ -1,7 +1,45 @@
 from rocket import Rocket
 
 class Flight:
-    def __init__(self, rocket, dt) -> None:
+    """
+    A class to simulate the flight of a rocket.
+
+    Attributes
+    ----------
+    rocket : Rocket
+        The rocket to simulate.
+    dt : float
+        The time step of the simulation (s).
+    t : float
+        The current time of the simulation (s).
+    z : float
+        The current altitude of the rocket (m).
+    vz : float
+        The current vertical velocity of the rocket (m/s).
+    az : float
+        The current vertical acceleration of the rocket (m/s^2).
+    phase : str
+        The current phase of the flight.
+    """
+    def __init__(
+        self,
+        rocket: Rocket,
+        dt: float
+    ) -> None:
+        """
+        Initialize the flight simulation.
+
+        Parameters
+        ----------
+        rocket : Rocket
+            The rocket to simulate.
+        dt : float
+            The time step of the simulation (s).
+
+        Returns
+        -------
+        None
+        """
         self.rocket = rocket
         self.dt = dt
         self.t = 0
