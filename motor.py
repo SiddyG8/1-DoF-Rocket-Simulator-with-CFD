84ba8f0a797baa08c5426ae295530566657fafa1
SiddyG8 <sgrov8@gmail.com>
2024-04-17 12:17:13 +1000

    added attributes T0 and tb in the __init__ function and completed the thrust function

--- motor.py
+++ motor.py
@@ -1,8 +1,12 @@
 from numpy import e, log
 
 class Motor:
-    def __init__(self, T0, tb, Isp) -> None:
-        pass
+    def __init__(self, T0, tb) -> None:
+        self.T0 = T0
+        self.tb = tb
+
+    def thrust(self, t) -> float:
+        return self.T0 * (1 - (10**-5) * (e**(log(10**5)/self.tb * t)))
 
 if __name__ == "__main__":
     pass
