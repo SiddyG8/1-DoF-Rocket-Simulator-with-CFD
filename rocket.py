1f5d12b7a172ca04c4b5980d1f11fb885dddc54b
SiddyG8 <sgrov8@gmail.com>
2024-04-17 13:14:39 +1000

    added fin and diameter attributes

--- rocket.py
+++ rocket.py
@@ -1,10 +1,12 @@
 from motor import Motor
 
 class Rocket:
-    def __init__(self, motor, wet_mass, fuel_mass) -> None:
+    def __init__(self, motor, wet_mass, fuel_mass, diameter, fins = None) -> None:
         self.motor = motor
+        self.fins = fins
         self.wet_mass = wet_mass
         self.fuel_mass = fuel_mass
+        self.diameter = diameter
 
     @property
     def dry_mass(self) -> float:
