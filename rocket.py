c506d81f5748bd9ba0a3fc48359da53c9fe4a361
SiddyG8 <sgrov8@gmail.com>
2024-05-10 23:51:22 +1000

    added a nose cone attribute to the init function

--- rocket.py
+++ rocket.py
@@ -1,9 +1,10 @@
 from motor import Motor
 
 class Rocket:
-    def __init__(self, motor, wet_mass, fuel_mass, diameter, fins = None) -> None:
+    def __init__(self, motor, wet_mass, fuel_mass, diameter, fins = None, nose_cone = None) -> None:
         self.motor = motor
         self.fins = fins
+        self.nose_cone = nose_cone
         self.wet_mass = wet_mass
         self.fuel_mass = fuel_mass
         self.diameter = diameter
