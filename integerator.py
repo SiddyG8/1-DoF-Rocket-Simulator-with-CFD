1644256850a1c1720261cdf6e0198feedf7cb572
SiddyG8 <sgrov8@gmail.com>
2024-05-11 22:29:45 +1000

    added code for an euler ode solving function and the __init__ function

--- integerator.py
+++ integerator.py
@@ -1,2 +1,17 @@
 class Integrator:
-    pass
+    def __init__(self, f, h):
+        self.f = f
+        self.h = h
+
+    def euler(self):
+        x = [self.x0]
+        y = [self.y0]
+
+        while True:
+            next_y = y[-1] + self.h * self.f(x[-1], y[-1])
+            y.append(next_y)
+            x.append(x[-1] + self.h)
+            if abs(y[-1] - y[-2]) > 1e100:
+                break
+
+        return x, y
