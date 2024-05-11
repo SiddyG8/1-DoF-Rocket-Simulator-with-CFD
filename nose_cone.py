cebb607a8bfca15354f4b4ea64d90c60708ef6be
SiddyG8 <sgrov8@gmail.com>
2024-05-11 14:47:00 +1000

    created an __init__ function and a wetted_area property

--- nose_cone.py
+++ nose_cone.py
@@ -1,2 +1,13 @@
+import math
+
 class NoseCone:
+    def __init__(self, length, diameter) -> None:
+        self.length = length
+        self.diameter = diameter
+
+    @property
+    def wetted_area(self) -> float:
+        return math.pi * (self.diameter / 2) ** 2
+
+if __name__ == "__main__":
     pass
