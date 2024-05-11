f11369f80a2118180672a6cfb294f017b1b629d6
SiddyG8 <sgrov8@gmail.com>
2024-05-11 14:47:14 +1000

    created a wetted_area property

--- rocket.py
+++ rocket.py
@@ -1,4 +1,4 @@
-from motor import Motor
+import math
 
 class Rocket:
     def __init__(self, motor, wet_mass, fuel_mass, diameter, fins = None, nose_cone = None) -> None:
@@ -12,6 +12,16 @@ class Rocket:
     @property
     def dry_mass(self) -> float:
         return self.wet_mass - self.fuel_mass
+    
+    @property
+    def wetted_area(self) -> float:
+        wetted_area = math.pi * (self.diameter / 2) ** 2
+        if self.nose_cone and self.nose_cone.diameter > self.diameter:
+            wetted_area = self.nose_cone.wetted_area
+        if self.fins:
+            wetted_area += self.fins.wetted_area
+        return wetted_area
+
 
 if __name__ == "__main__":
     pass
