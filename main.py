392f14d9c2655a8309388c211cb9f81f9907709a
SiddyG8 <sgrov8@gmail.com>
2024-05-11 22:30:37 +1000

    setup base code for creating a rocket, then a flight, then plotting the results of the simulation for altitude against time

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from rocket import Rocket
+from flight import Flight
+
 class Main:
     def __init__(self) -> None:
-        pass
+        self.rocket = Rocket(
+            mass=27.201, # kg
+            fuel_mass=4.766, # kg
+            diameter=0.156, # meters
+            T0=3983, # Newtons
+            tb=2.9 # Seconds
+        )
+        self.flight = Flight(self.rocket)
+        self.flight.plot("time", "altitude", "Time (s)", "Altitude (m)")
+
 
 if __name__ == "__main__":
     Main()
