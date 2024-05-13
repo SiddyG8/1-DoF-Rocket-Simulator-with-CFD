2c16e4deb1d9fadc39ec8d50a1bb2cb33da2acc8
SiddyG8 <sgrov8@gmail.com>
2024-05-13 18:45:20 +1000

    removed the Main class as it was not needed, also configured the code to show plots of the forces the rocket is subject to

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from rocket import Rocket
 from flight import Flight
+import matplotlib.pyplot as plt
 
-class Main:
-    def __init__(self) -> None:
-        self.rocket = Rocket(
-            mass=27.201, # kg
-            fuel_mass=4.766, # kg
-            diameter=0.156, # meters
-            T0=3983, # Newtons
-            tb=2.9 # Seconds
-        )
-        self.flight = Flight(self.rocket)
-        self.flight.plot("time", "altitude", "Time (s)", "Altitude (m)")
+
+def main() -> None:
+    rocket = Rocket(
+        mass=27.201, # kg
+        fuel_mass=4.766, # kg
+        diameter=0.156, # meters
+        T0=3415, # Newtons
+        tb=2.9 # Seconds
+    )
+    flight = Flight(rocket)
+    # print(max(flight.z))
+    # plt.plot(flight.t, flight.vz)
+    plt.plot(flight.t, flight.thrust_forces, label="Thrust")
+    plt.plot(flight.t, flight.drag_forces, label="Drag")
+    plt.plot(flight.t, flight.gravitional_forces, label="Gravitation")
+    print(max(flight.z))
+    print(max(flight.drag_forces))
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
-    Main()
+    main()
