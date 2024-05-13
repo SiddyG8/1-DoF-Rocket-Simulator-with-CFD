c05a2f00181171e2af8fe185dc7f60a27f356d2c
SiddyG8 <sgrov8@gmail.com>
2024-05-13 18:43:41 +1000

    removed all the equation calculations into a seperate class and file and finished the simulation function (flight phases still need to be added)

--- flight.py
+++ flight.py
@@ -1,74 +1,81 @@
 from rocket import Rocket
 import constants as c
-from integerator import euler
-from numpy import e, sin, log10
-import math
+import functions as f
+import matplotlib.pyplot as plt
 
 class Flight:
     def __init__(self, rocket: Rocket, dt: float = 0.05) -> None:
         self.rocket = rocket
         self.dt = dt
-        self.step = 0
         self.t = [0]
         self.z = [0]
         self.vz = [0]
-        self.az = [0]
-        self.M = [0]
-        self.g = [c.g0]
+        self.az = [self.rocket.thrust(0) / self.rocket.mass]
+        self.masses = [self.rocket.mass]
+        self.M = [f.calculate_mach_number(0, c.T0)]
+        self.gravitional_forces = [-c.g0 * self.rocket.mass]
         self.drag_forces = [0]
         self.drag_coefficients = [0]
         self.air_pressures = [c.p0]
         self.air_densities = [c.rho0]
         self.air_temperatures = [c.T0]
-        self.thrust_forces = [0]
+        self.thrust_forces = [self.rocket.thrust(0)]
         self.phases = ["on pad"]
 
-        # Run the simulation
+        # Simulate the flight based off initial parameters
         self.simulate()
 
-    def calculate_gravitational_acceleration(self, step) -> float:
-        return c.GM / (c.RE + self.z[step]) ** 2
-
-    def calculate_gravitational_force(self, step) -> float:
-        return -self.rocket.mass * self.g[step]
-
-    def calculate_thrust_force(self, step) -> float:
-        return self.rocket.thrust(self.t[step])
-
-    def calculate_mach_number(self, step) -> float:
-        return abs(self.vz[step]) / math.sqrt(c.gamma * c.R * self.air_temperatures[step])
-
-    def calculate_altitude(self, step) -> float:
-        return self.z[step]
-
-    def calculate_air_temperature(self, step) -> float:
-        return c.T0 - c.L * self.z[step]
-
-    def calculate_air_density(self, step) -> float:
-        return self.air_pressures[step] / (c.R * self.air_temperatures[step])
-
-    def calculate_pressure(self, step) -> float:
-        return c.p0 * (self.air_temperatures[step] / c.T0) ** (-c.g0 / (c.R * c.L))
-
-    def calculate_drag_coefficient(self, step) -> float:
-        return pow(e, -1.2 * self.M[step]) * sin(self.M[step]) + (self.M[step] / 6) * log10(self.M[step] + 1)
-
-    def calculate_drag_force(self, step) -> float:
-        return 0.5 * self.air_densities[step] * self.vz[step] * self.vz[step] * self.rocket.wetted_area * self.drag_coefficients[step]
-    
     def f(self, t, s, v):
-        self.step += 1
-        self.t.append(t)
-        self.z.append(s)
-        self.vz.append(v)
-        return self.calculate_thrust_force(self.step) - self.calculate_drag_force(self.step) - self.calculate_gravitational_force(self.step)
+        air_temperature = f.calculate_air_temperature(s)
+        air_pressure = f.calculate_air_pressure(air_temperature)
+        air_density = f.calculate_air_density(air_pressure, air_temperature)
+        mach_number = f.calculate_mach_number(v, air_temperature)
+        drag_coefficient = f.calculate_drag_coefficient(mach_number)
+        net_force = self.rocket.thrust(t) + f.calculate_drag_force(air_density, v, self.rocket.wetted_area, drag_coefficient) + f.calculate_gravitational_force(s, self.masses[-1])
+        return net_force / self.masses[-1]
 
     def simulate(self) -> None:
-        while self.phases[self.step] != "landed":
-            euler(self.f, self.dt)
+        while "landed" not in self.phases:
+            t = self.t[-1] + self.dt
+            z = self.z[-1] + self.dt * self.vz[-1]
+            vz = self.vz[-1] + self.dt * self.az[-1]
+            az = self.f(t, z, vz)
+            mass = f.calculate_mass(self.rocket.mass, self.rocket.fuel_mass, self.rocket.delta_mass, t)
+            gravitional_force = f.calculate_gravitational_force(z, mass)
+            air_temperature = f.calculate_air_temperature(z)
+            air_pressure = f.calculate_air_pressure(air_temperature)
+            air_density = f.calculate_air_density(air_pressure, air_temperature)
+            M = f.calculate_mach_number(vz, air_temperature)
+            cd = f.calculate_drag_coefficient(M)
+            drag_force = f.calculate_drag_force(air_density, vz, self.rocket.wetted_area, cd)
+            thrust_force = self.rocket.thrust(t)
 
-    def plot(*variables, x_label, y_label) -> None:
-        pass
+            if z < 0:
+                phase = "landed"
+            else:
+                phase = "on pad"
+
+            self.t.append(t)
+            self.z.append(z)
+            self.vz.append(vz)
+            self.az.append(az)
+            self.masses.append(mass)
+            self.gravitional_forces.append(gravitional_force)
+            self.air_temperatures.append(air_temperature)
+            self.air_densities.append(air_density)
+            self.air_pressures.append(air_pressure)
+            self.M.append(M)
+            self.drag_coefficients.append(cd)
+            self.drag_forces.append(drag_force)
+            self.thrust_forces.append(thrust_force)
+            self.phases.append(phase)
+
+    def plot(self, x, y, x_label, y_label) -> None:
+        plt.plot(x, y)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.show()
+
 
 if __name__ == "__main__":
     pass
