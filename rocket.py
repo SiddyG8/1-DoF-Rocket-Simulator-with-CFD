762043ab88e17f3b2534c50d4e738273c9216ab1
SiddyG8 <sgrov8@gmail.com>
2024-05-14 23:39:06 +1000

    moved motor parameters to its class

--- rocket.py
+++ rocket.py
@@ -1,12 +1,11 @@
 from math import pi, e, log
 
 class Rocket:
-    def __init__(self, mass, fuel_mass, diameter, T0, tb) -> None:
+    def __init__(self, mass, fuel_mass, diameter, motor) -> None:
         self.mass = mass
         self.fuel_mass = fuel_mass
         self.diameter = diameter
-        self.T0 = T0
-        self.tb = tb
+        self.motor = motor
 
     @property
     def delta_mass(self) -> float:
@@ -15,9 +14,6 @@ class Rocket:
     @property
     def wetted_area(self) -> float:
         return pi * (self.diameter / 2) ** 2
-
-    def thrust(self, t) -> float:
-        return max(0, self.T0 * (1 - (10**-5) * (e**(log(10**5)/self.tb * t))))
 
 if __name__ == "__main__":
     pass
