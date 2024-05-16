7208ab0bebd3c9da0a71c890de6a4deeb4657a7a
SiddyG8 <sgrov8@gmail.com>
2024-05-16 17:32:09 +1000

    created a constructor and the wetted_area property

--- fins.py
+++ fins.py
@@ -1,2 +1,12 @@
 class Fins:
-    pass
+    def __init__(self, num_fins, width, root_chord, tip_chord, span, sweep_angle) -> None:
+        self.num_fins = num_fins
+        self.width = width
+        self.root_chord = root_chord
+        self.tip_chord = tip_chord
+        self.span = span
+        self.sweep_angle = sweep_angle
+
+    @property
+    def wetted_area(self) -> float:
+        return self.num_fins * self.width * self.span
