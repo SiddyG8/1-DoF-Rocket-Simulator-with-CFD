d3ae04f1a009a4063b782e944ebf781d66c2ca4e
SiddyG8 <sgrov8@gmail.com>
2024-07-14 20:26:22 +1000

    using the flight functions for summary of the flight instead of calculating them manually

--- main.py
+++ main.py
@@ -38,35 +38,12 @@ def main() -> None:
     rocket.attach_nose_cone(nose_cone)
     rocket.attach_fins(fins)
 
-    flight = Flight(rocket, drag_data="Data/aggregated_coefficients.csv")
-
-    # Printing flight statistics
-    print(f"Apogee: {max(flight.z):.2f} meters at {flight.t[flight.z.index(max(flight.z))]:.2f} seconds")
-    print(f"Max velocity: {max(flight.vz):.2f} m/s at {flight.t[flight.vz.index(max(flight.vz))]:.2f} seconds")
-    print(f"Flight time: {flight.t[-1]:.2f} seconds")
-    print(f"Max Q : {max(flight.dynamic_pressures)/1000:.2f} kPa at {flight.t[flight.dynamic_pressures.index(max(flight.dynamic_pressures))]:.2f} seconds")
-    print(f"Starting TWR: {flight.twr[0]:.2f}")
-    print(f"Off Rod Velocity: {flight.off_rod_velocity(5)[1]:.2f} m/s at {flight.off_rod_velocity(5)[0]:.2f} seconds")
-
-    # Forces plot
-    flight.plot(
-        "t",
-        ("thrust_forces", "Thrust"),
-        ("drag_forces", "Drag"),
-        ("gravitational_forces", "Gravitation"),
-        x_label="Time (s)",
-        y_label="Force (N)"
-    )
-
-    # Altitude plot
-    flight.plot(
-        "t",
-        ("z", "Altitude"),
-        ("vz", "Velocity (m/s)"),
-        ("az", "Acceleration (m/s^2)"),
-        x_label="Time (s)",
-        y_label="Altitude (m)"
-    )
+    # flight = Flight(rocket, drag_data="Data/aggregated_coefficients.csv")
+    flight = Flight(rocket)
+    flight.summary()
+    flight.write_to_file("Data/sim_data.csv")
+    flight.plot("times", ("thrust_forces", "Thrust"), ("drag_forces", "Drag"), ("gravitational_forces", "Weight"), x_label="Time (s)", y_label="Force (N)")
+    flight.plot("times", ("altitudes", "Altitude"), ("velocities", "Velocity (m/s)"), ("accelerations", "Acceleration (m/s^2)"), x_label="Time (s)", y_label="Altitude (m)")
 
 if __name__ == "__main__":
     main()
